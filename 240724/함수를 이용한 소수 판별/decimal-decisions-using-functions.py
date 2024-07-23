e16296a48ae4c8a9d1ebc8956627c448fb9ab4fb
IronDragon-Moon <irondragon2729@gmail.com>
2024-07-24 01:26:02 +0900

    Title: 함수를 이용한 소수 판별, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240724/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,12 @@
+a,b = map(int,input().split())
+result = 0
+def is_sosu(n):
+    for i in range(2,n):
+        if n%i ==0:
+            return False
+    return True
+
+for i in range(a,b+1):
+    if is_sosu(i):
+        result+=i
+print(result)
