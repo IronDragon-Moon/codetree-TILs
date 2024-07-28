472c30bad0697d632b4f0022bbedf616ec301db3
IronDragon-Moon <irondragon2729@gmail.com>
2024-07-29 01:08:40 +0900

    Title: 특정 구간의 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240729/특정 구간의 합/sum-of-specific-intervals.py
@@ -0,0 +1,11 @@
+n,m = map(int,input().split())
+list_ =[0]+ list(map(int,input().split()))
+def froma1_toa2(a,b):
+    result =0
+    for i in range(a,b+1):
+        result += list_[i]
+    return result
+
+for i in range(m):
+    a,b = map(int().split())
+    print(froma1_toa2(a,b))
