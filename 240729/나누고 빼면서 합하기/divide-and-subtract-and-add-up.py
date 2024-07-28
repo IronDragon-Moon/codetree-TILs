a9f566f02825c6118fc65ca29782836e048deb50
IronDragon-Moon <irondragon2729@gmail.com>
2024-07-29 00:59:03 +0900

    Title: 나누고 빼면서 합하기, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240729/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
+++ 240729/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -3,15 +3,14 @@ list_ = list(map(int,input().split()))
 def fun():
     global m
     result = 0
-    if m==1:
-        return list_[0]
+
     while True:
         result += list_[m-1]
+        if m==1:
+            return result
         if m%2==1:
             m -= 1
         elif m%2==0:
             m //= 2
-        if m==1:
-            result += list_[m-1]
-            return result
+        
 print(fun())
